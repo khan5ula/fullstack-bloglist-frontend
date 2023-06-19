7a3f31b5d13a12449af699c012e3f7389014abc3
Kristian Hannula <kristian.hannula@proton.me>
2023-06-19 17:07:48 +0300

    Task 5.18 done, added tests ffor login

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -1,7 +1,9 @@
 describe('Blog app', function () {
   beforeEach(function () {
+    /* Delete blog posts and users */
     cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`)
 
+    /* Create and post new user */
     const user = {
       name: 'John Doe',
       username: 'johndoe',
@@ -10,6 +12,7 @@ describe('Blog app', function () {
 
     cy.request('POST', `${Cypress.env('BACKEND')}/users`, user)
 
+    /* Navigate to main page */
     cy.visit('')
   })
 
@@ -17,9 +20,43 @@ describe('Blog app', function () {
     cy.contains('Log in to application')
   })
 
-  it('login form is shown', function() {
+  it('login form is shown', function () {
     cy.get('#username-form')
     cy.get('#password-form')
     cy.contains('login')
+  })
+
+  describe('login', function () {
+    it('succeeds with correct credentials', function () {
+
+      /* Submit correct user info */
+      cy.get('#username-form').type('johndoe')
+      cy.get('#password-form').type('salasana')
+      cy.get('#login-button').click()
+
+      /* Expect success */
+      cy.contains('John Doe logged in')
+    })
+
+    it('fails with wrong credentials', function () {
+
+      /* Submit invalid username */
+      cy.get('#username-form').type('janedoe')
+      cy.get('#password-form').type('salasana')
+      cy.get('#login-button').click()
+
+      /* Expect failure */
+      cy.contains('wrong username or password')
+
+      /* Submit invalid password */
+      cy.get('#username-form').clear()
+      cy.get('#username-form').type('johndoe')
+      cy.get('#password-form').clear()
+      cy.get('#password-form').type('password')
+      cy.get('#login-button').click()
+
+      /* Expect failure */
+      cy.contains('wrong username or password')
+    })
   })
 })
